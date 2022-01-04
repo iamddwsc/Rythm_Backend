d9992a00233580d9bff54ce83d7655f3633114ab
ddwsc <ddwsc.1501@gmail.com>
2022-01-04 20:13:05 +0700

    fixed albumscontroller

--- app/Http/Controllers/Api/AlbumsController.php
+++ app/Http/Controllers/Api/AlbumsController.php
@@ -27,31 +27,64 @@ class AlbumsController extends Controller
                     function (Crawler $node) {
                         $song_image = $node->filter('#companion_cover > img')->attr('src');
                         $song_title = $node->filter('.card-body .card-title')->text();
-                        $song_artist = $node->filter('.list-unstyled > li')->eq(0)->filter('a')->text();
-                        $song_of_album = $node->filter('.list-unstyled > li')->eq(1)->filter('a')->text();
-                        $song_date_release = $node->filter('.list-unstyled > li')->eq(3)->text();
-                        $song_date_release = substr($song_date_release,18);
-                        $child = $node->filter('ul.list-unstyled > li .download_item')->count();
-                        if ($child >= 5) {
-                            $mp3_url_128 = $node->filter('ul.list-unstyled > li .download_item')->eq(1)->attr('href');
-                            $mp3_url_320 = $node->filter('ul.list-unstyled > li .download_item')->eq(2)->attr('href');
-                            $mp3_url_500 = $node->filter('ul.list-unstyled > li .download_item')->eq(3)->attr('href');
+                        $li_count = $node->filter('.card-body > .list-unstyled > li')->count();
+                        if ($li_count < 4) {
+                            $song_singer_count = $node->filter('.list-unstyled > li')->eq(0)->filter('a')->count(); 
+                            $song_singer = $node->filter('.list-unstyled > li')->eq(0)->filter('a');
+                            $z = array();
+                            for ($i = 0; $i < $song_singer_count; $i++) {
+                                array_push($z, $song_singer->eq($i)->text());
+                            }
+                            $z = implode(', ', $z);
+                            $song_of_album = $node->filter('.list-unstyled > li')->eq(1)->filter('a')->text();
+                            $song_date_release = $node->filter('.list-unstyled > li')->eq(2)->text();
+                            $song_date_release = substr($song_date_release,18);
+                            $songwriter = '';
                         } else {
-                            $mp3_url_128 = $node->filter('ul.list-unstyled > li .download_item')->eq(1)->attr('href');
-                            $mp3_url_320 = '';
-                            $mp3_url_500 = '';
+                            $song_singer_count = $node->filter('.list-unstyled > li')->eq(0)->filter('a')->count();                   
+                            $song_singer = $node->filter('.list-unstyled > li')->eq(0)->filter('a');
+                            $z = array();
+                            for ($i = 0; $i < $song_singer_count; $i++) {
+                                //$data[$i] = $song_singer->eq($i)->text();
+                                array_push($z, $song_singer->eq($i)->text());
+                            }
+                            $z = implode(', ', $z);
+                            $songwriter = $node->filter('.list-unstyled > li')->eq(1)->filter('a')->text();
+                            $song_of_album = $node->filter('.list-unstyled > li')->eq(2)->filter('a')->text();
+                            $song_date_release = $node->filter('.list-unstyled > li')->eq(3)->text();
+                            $song_date_release = substr($song_date_release,18);
                         }
+                        //$song_artist = $node->filter('.list-unstyled > li')->eq(0)->filter('a')->text();
+                        
+                        
+                        // $child = $node->filter('ul.list-unstyled > li .download_item')->count();
+                        // if ($child >= 5) {
+                        $mp3_url_128 = $node->filter('ul.list-unstyled > li .download_item')->eq(1)->attr('href');
+                        $mp3_url_320 = str_replace('/128/', '/320/', $mp3_url_128);
+                        $mp3_url_500 = str_replace(array('/128/', '.mp3'), array('/m4a/','.m4a'), $mp3_url_128);
+                        $lossless = str_replace(array('/128/', '.mp3'), array('/flac/','.flac'), $mp3_url_128);
+                            //$mp3_url_320 = $node->filter('ul.list-unstyled > li .download_item')->eq(2)->attr('href');
+                            //$mp3_url_500 = $node->filter('ul.list-unstyled > li .download_item')->eq(3)->attr('href');
+                        // } else {
+                        //     $mp3_url_128 = $node->filter('ul.list-unstyled > li .download_item')->eq(1)->attr('href');
+                        //     $pos = strpos($mp3_url_128, '/128/');
+                        //     $mp3_url_320 = '';
+                        //     $mp3_url_500 = '';
+                        // }
                         // $mp3_url_500 = $node->filter('ul.list-unstyled > li .download_item')->eq(3)->attr('href');
                         $song_info_arr = Array(
                             'song_image' => $song_image,
                             'song_title' => $song_title,
-                            'song_artist' => $song_artist,
+                            'song_singer' => $z,
+                            'songwriter' => $songwriter,
                             'song_of_album' => $song_of_album,
                             'song_date_release' => $song_date_release,
                             'mp3_url_128' => $mp3_url_128,
                             'mp3_url_320' => $mp3_url_320,
                             'mp3_url_500' => $mp3_url_500,
-                            'child' => $child
+                            'lossless' => $lossless
+                            //'pos' => $pos
+                            //'child' => $li_count
                         );
                         return $song_info_arr;
                     }
